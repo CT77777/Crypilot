da9662db4a5cb661fd8c2c8a601a6a95ff80e1a0
CT77777 <chanyihcheng@gmail.com>
2023-07-12 22:19:30 +0800

    optimize and debug

--- client/js/home.js
+++ client/js/home.js
@@ -1,1 +1,31 @@
 import * as logIn from "./modules/logIn.js";
+
+const navUserLinks = document.querySelectorAll(".nav-link-user");
+
+navUserLinks.forEach((userLink) => {
+  userLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    const jwt = Cookies.get("JWT");
+
+    if (jwt === undefined) {
+      iziToast.show({
+        theme: "dark",
+        iconUrl: "../images/error.png",
+        title: `Please log in first`,
+        titleSize: 18,
+        messageSize: 18,
+        position: "topCenter",
+        maxWidth: 500,
+        timeout: 3000,
+        pauseOnHover: true,
+        drag: true,
+        displayMode: 2,
+      });
+
+      return;
+    } else {
+    }
+
+    location.assign(userLink.href);
+  });
+});
