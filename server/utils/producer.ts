ca1f7067851679e6ecac3e902c7daf0fb3f1dce7
CT77777 <chanyihcheng@gmail.com>
2023-07-27 15:28:16 +0800

    add home/user/gpt testing scripts

--- server/utils/producer.ts
+++ server/utils/producer.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const rabbitMqClient: Connection = await client.connect(
+export const rabbitMqClient: Connection = await client.connect(
   `amqps://${process.env.AMQP_USERNAME}:${process.env.AMQP_PASSWORD}@cougar.rmq.cloudamqp.com/nusunbji`
 );
 
@@ -12,10 +12,8 @@ export const channel: Channel = await rabbitMqClient.createChannel();
 export async function assertQueues(channel: Channel) {
   await channel.assertQueue("buyEth_Queue", { durable: true });
   console.log(`buyEth_Queue active...`);
-
   await channel.assertQueue("swapEthToErc20_Queue", { durable: true });
   console.log(`swapEthToErc20_Queue active...`);
-
   await channel.assertQueue("swapErc20ToEth_Queue", { durable: true });
   console.log(`swapErc20ToEth_Queue active...`);
 }
